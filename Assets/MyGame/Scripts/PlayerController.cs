5fcd259bf2930ef254bea37decb4c573829c44a9
MatteoEtzer <matteoetzer@gmx.at>
2021-03-23 10:24:46 +0100

    refac: deleted start method

--- Assets/MyGame/Scripts/PlayerController.cs
+++ Assets/MyGame/Scripts/PlayerController.cs
@@ -16,11 +16,6 @@ public class PlayerController : MonoBehaviour
         anim = GetComponent<Animator>();
     }
 
-    void Start()
-    {
-        
-    }
-
     void Update()
     {
         if (Input.GetMouseButton(0) && !gameOver && !gameOver && !gameOver)
